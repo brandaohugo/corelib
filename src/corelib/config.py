2a00fbdc780c4849afb282e321abf06cc82f46a1
Hugo Brandão <hrbrandao@proton.me>
2025-08-19 10:07:17 +0200

    remove unused local env var

--- src/corelib/config.py
+++ src/corelib/config.py
@@ -32,23 +32,15 @@ class Settings(BaseSettings):
         extra="ignore",
     )
     API_V1_STR: str = "/api/v1"
-    SECRET_KEY: str = secrets.token_urlsafe(32)
+    SECRET_KEY: str
     # 60 minutes * 24 hours * 8 days = 8 days
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    # ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     FRONTEND_HOST: str = "http://localhost:5173"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 
     BACKEND_CORS_ORIGINS: Annotated[
         list[AnyUrl] | str, BeforeValidator(parse_cors)
     ] = []
-
-    AZ_REDIRECT_URI: str
-    AZ_CLIENT: str
-    AZ_SECRET: str
-    AZ_TENANT: str
-
-    PIPE_ACCESS_TOKEN: str
-    PIPE_URL: str
 
     @computed_field  # type: ignore[prop-decorator]
     @property
@@ -61,7 +53,7 @@ class Settings(BaseSettings):
     # SENTRY_DSN: HttpUrl | None = None
     POSTGRES_SERVER: str
     POSTGRES_PORT: int = 5432
-    POSTGRES_USER: str
+    POSTGRES_USER: str = "postgres"
     POSTGRES_PASSWORD: str = ""
     POSTGRES_DB: str = ""
 
@@ -77,31 +69,31 @@ class Settings(BaseSettings):
             path=self.POSTGRES_DB,
         )
 
-    SMTP_TLS: bool = True
-    SMTP_SSL: bool = False
-    SMTP_PORT: int = 587
-    SMTP_HOST: str | None = None
-    SMTP_USER: str | None = None
-    SMTP_PASSWORD: str | None = None
-    EMAILS_FROM_EMAIL: EmailStr | None = None
-    EMAILS_FROM_NAME: EmailStr | None = None
+    # SMTP_TLS: bool = True
+    # SMTP_SSL: bool = False
+    # SMTP_PORT: int = 587
+    # SMTP_HOST: str | None = None
+    # SMTP_USER: str | None = None
+    # SMTP_PASSWORD: str | None = None
+    # EMAILS_FROM_EMAIL: EmailStr | None = None
+    # EMAILS_FROM_NAME: EmailStr | None = None
 
-    @model_validator(mode="after")
-    def _set_default_emails_from(self) -> Self:
-        if not self.EMAILS_FROM_NAME:
-            self.EMAILS_FROM_NAME = self.PROJECT_NAME
-        return self
+    # @model_validator(mode="after")
+    # def _set_default_emails_from(self) -> Self:
+    #     if not self.EMAILS_FROM_NAME:
+    #         self.EMAILS_FROM_NAME = self.PROJECT_NAME
+    #     return self
 
-    EMAIL_RESET_TOKEN_EXPIRE_HOURS: int = 48
+    # EMAIL_RESET_TOKEN_EXPIRE_HOURS: int = 48
 
-    @computed_field  # type: ignore[prop-decorator]
-    @property
-    def emails_enabled(self) -> bool:
-        return bool(self.SMTP_HOST and self.EMAILS_FROM_EMAIL)
+    # @computed_field  # type: ignore[prop-decorator]
+    # @property
+    # def emails_enabled(self) -> bool:
+    #     return bool(self.SMTP_HOST and self.EMAILS_FROM_EMAIL)
 
-    EMAIL_TEST_USER: EmailStr = "test@example.com"
-    FIRST_SUPERUSER: EmailStr
-    FIRST_SUPERUSER_PASSWORD: str
+    # EMAIL_TEST_USER: EmailStr = "test@example.com"
+    # FIRST_SUPERUSER: EmailStr
+    # FIRST_SUPERUSER_PASSWORD: str
 
     def _check_default_secret(self, var_name: str, value: str | None) -> None:
         if value == "changethis":
