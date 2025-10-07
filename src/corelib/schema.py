de4a37175568f81124caf4e15e0521910b4c4ace
Hugo Brandão <hrbrandao@proton.me>
2025-10-07 12:29:55 +0200

    setup base schema in corelib

---
+++ src/corelib/schema.py
@@ -0,0 +1,53 @@
+from .types import DealBase, CustomerBase, ContactBase
+import uuid
+
+from sqlmodel import SQLModel
+
+
+# DEAL SCHEMA
+class DealPublic(DealBase, SQLModel):
+    pass
+
+
+class DealInListPublic(DealPublic):
+    pass
+
+
+class DealsPublic(SQLModel):
+    data: list[DealInListPublic]
+    count: int
+
+class DealUpdate(DealBase, SQLModel):
+    pass
+
+
+# CUSTOMER SCHEMA
+class CustomerPublic(CustomerBase, SQLModel):
+    id: uuid.UUID
+
+
+class CustomerInListPublic(CustomerPublic):
+    pass
+
+class CustomersPublic(CustomerBase, SQLModel):
+    data: list[CustomerInListPublic]
+    count: int
+
+class CustomerUpdate(CustomerBase, SQLModel):
+    pass
+
+
+# CONTACT SCHEMA
+class ContactPublic(ContactBase, SQLModel):
+    id: uuid.UUID
+
+
+class ContactInListPublic(ContactPublic):
+    pass
+
+class ContactsPublic(ContactBase, SQLModel):
+    data: list[ContactInListPublic]
+    count: int
+
+class ContactUpdate(ContactBase, SQLModel):
+    pass
